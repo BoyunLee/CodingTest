51da30de95c88e5554fd45822a6c6e920d41250c
이보윤 <qhdbs0802@naver.com>
2024-06-25 14:23:54 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 272 ms, Memory: 21080 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -10,16 +10,19 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             a[i] = sc.nextInt();
-            d[i] = a[i]; // 초기화: 각 원소 자체가 하나로 이루어진 수열이 가장 긴 증가하는 수열일 수 있음
+            // 초기화: 각 원소 자체가 하나로 이루어진 수열이 가장 긴 증가하는 수열일 수 있음
+            d[i] = a[i];
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j < i; j++) {
                 if (a[j] < a[i]) {
-                    d[i] = Math.max(d[i], d[j] + a[i]); // 부분 수열의 합이 최대가 되도록 업데이트
+                    // 부분 수열의 합이 최대가 되도록 업데이트
+                    d[i] = Math.max(d[i], d[j] + a[i]);
                 }
             }
-            max = Math.max(max, d[i]); // 최대값 업데이트
+            // 최대값 업데이트
+            max = Math.max(max, d[i]);
         }
 
         System.out.println(max);
