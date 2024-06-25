76d32d601df5e4f2f769a320efa783cb55329495
이보윤 <qhdbs0802@naver.com>
2024-06-25 14:01:10 +0900

    [level 2] Title: 올바른 괄호, Time: 6.62 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+class Solution {
+        boolean solution(String s) {
+            boolean answer = true;
+
+            int count = 0;
+
+            for (int i=0; i<s.length(); i++) {
+                if (s.charAt(i) == '('){
+                    count++;
+                } else {
+                    count--;
+                }    
+                if (count < 0) {
+                    answer = false;
+                }
+            }
+            if (count != 0) {
+                answer = false;
+            }
+            return answer;
+        }
+    }
