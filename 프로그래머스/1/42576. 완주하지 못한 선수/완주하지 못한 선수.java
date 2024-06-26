5a18a737c62b5d3d21c8310aed0bab892a6572a2
이보윤 <qhdbs0802@naver.com>
2024-06-26 17:27:00 +0900

    완주하지 못한 선수.java

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,11 +1,10 @@
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
+        
         // participant 배열 순회
         for (int i = 0; i < participant.length; i++) {
             String player = participant[i];
@@ -24,15 +23,22 @@ class Solution {
             }
         }
 
-        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+        // 해시맵에서 값이 0이 아닌 참가자 찾기
+            for (int i = 0; i < participant.length; i++) {
+                String player = participant[i];
+                if (map.get(player) > 0) {
+                    answer = player;
+                    break;
 
-        while(iterator.hasNext()){
-            Map.Entry<String, Integer> entry = iterator.next();
-            if (entry.getValue() != 0){
-                answer = entry.getKey();
-                break;
+        // Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+
+        // while(iterator.hasNext()){
+        //     Map.Entry<String, Integer> entry = iterator.next();
+        //     if (entry.getValue() != 0){
+        //         answer = entry.getKey();
+        //         break;
+                }
             }
-        }
         return answer;
     }
 }
