a7a413810f743a7f59d764d39ec7ae64171862c8
이보윤 <qhdbs0802@naver.com>
2024-06-28 14:49:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 169.67 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,44 +1,27 @@
-import java.util.HashMap;
 
+import java.util.HashMap;
+    
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
-        
+
         // participant 배열 순회
-        for (int i = 0; i < participant.length; i++) {
-            String player = participant[i];
-            if (map.containsKey(player)) {
-                map.put(player, map.get(player) + 1);
-            } else {
-                map.put(player, 1);
-            }
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
 
         // completion 배열 순회
-        for (int i = 0; i < completion.length; i++) {
-            String player = completion[i];
-            if (map.containsKey(player)) {
-                map.put(player, map.get(player) - 1);
-            }
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
         }
 
         // 해시맵에서 값이 0이 아닌 참가자 찾기
-            for (int i = 0; i < participant.length; i++) {
-                String player = participant[i];
-                if (map.get(player) > 0) {
-                    answer = player;
-                    break;
-
-        // Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-
-        // while(iterator.hasNext()){
-        //     Map.Entry<String, Integer> entry = iterator.next();
-        //     if (entry.getValue() != 0){
-        //         answer = entry.getKey();
-        //         break;
-                }
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return key;
             }
+        }
         return answer;
     }
 }
