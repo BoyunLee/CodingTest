8cfdbb24b65d446db22ee1d2bc0aedc3ad7c8f73
이보윤 <qhdbs0802@naver.com>
2024-07-04 09:47:36 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.30 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        // 문자열 회전
+        for (int i = 0; i < s.length(); i++) {
+            String rotatedStr = s.substring(i) + s.substring(0, i);
+            if (isValid(rotatedStr)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    // 괄호 짝 검사 메서드
+    private boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (!matches(top, c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    // 괄호 짝 확인 메서드
+    private boolean matches(char open, char close) {
+        return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
+    }
+}
