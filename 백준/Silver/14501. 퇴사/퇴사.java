43cc9162fa211ca708374ae72428ae993dd73cb9
이보윤 <qhdbs0802@naver.com>
2024-07-08 22:33:24 +0900

    [Silver III] Title: 퇴사, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        sc.nextLine(); // 개행 문자 처리
+        sc.nextLine();
 
         // n개의 숫자 쌍을 저장할 배열 선언
         int[][] days = new int[N][2];
@@ -13,7 +13,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             days[i][0] = sc.nextInt();
             days[i][1] = sc.nextInt();
-            sc.nextLine(); // 개행 문자 처리
+            sc.nextLine();
         }
 
         int[] dp = new int[N+1];
