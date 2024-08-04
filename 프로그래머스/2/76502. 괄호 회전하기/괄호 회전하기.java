a404a4bacb4f17e4e01588df10c9358a7da2bd43
이보윤 <qhdbs0802@naver.com>
2024-08-04 23:46:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 34.29 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,51 +1,39 @@
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        
-    //     // 회전 대신 문자열 두 번 반복하기
-    //     String A = s + s; 
-    //     for(int i=0; i<s.length(); i++) {
-    //          String rotatedStr = A.substring(i, i + s.length());
-    //         if (isValid(rotatedStr)) {
-    //             answer++;
-    //         }
-    //     }
-    //     return answer;
-    // }
-
-        // 문자열 회전
-        for (int i = 0; i < s.length(); i++) {
-            String rotatedStr = s.substring(i) + s.substring(0, i);
-            if (isValid(rotatedStr)) {
-                answer++;
+        String A = s + s;
+        for(int i=0; i<s.length(); i++) {
+            String target = A.substring(i, i + s.length());
+            if(isVaild(target)) {
+                answer ++;
             }
         }
+        
         return answer;
     }
-
-    // 괄호 짝 검사 메서드
-    private boolean isValid(String str) {
+    
+    private boolean isVaild(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char c : str.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
+
+        for (char current : s.toCharArray()) {
+            if (current == '(' ||  current == '{' ||  current == '[') {
+                stack.push(current);
             } else {
                 if (stack.isEmpty()) {
                     return false;
                 }
-                char top = stack.pop();
-                if (!matches(top, c)) {
-                    return false;
+
+                char target = stack.pop();
+                if ((target == '(' && current != ')') || 
+                    (target == '{' && current != '}') || 
+                    (target == '[' && current != ']')) {
+                        return false;
                 }
             }
         }
         return stack.isEmpty();
-    }
-
-    // 괄호 짝 확인 메서드
-    private boolean matches(char open, char close) {
-        return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
     }
 }
