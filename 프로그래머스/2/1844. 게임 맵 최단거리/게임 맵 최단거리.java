5f675cfdc91f0944805a88736fac798a35c72c60
이보윤 <qhdbs0802@naver.com>
2024-08-07 10:10:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.89 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,36 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
+        int answer = -1;
         int n = maps.length;
         int m = maps[0].length;
         boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new ArrayDeque<>();
-
-        queue.add(new int[]{ 0, 0, 1 });
+        
+        queue.add(new int[] {0, 0, 1});
         visited[0][0] = true;
         
-        int[] dr = { 0, 1, 0, -1 };
-        int[] dc = { -1, 0, 1, 0 };
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
         
-        while (!queue.isEmpty()) {
-            int[] cur = queue.remove();
+        while(!queue.isEmpty()) {
+            int cur[] = queue.remove();
             int r = cur[0], c = cur[1], dist = cur[2];
             
-            if (r == n - 1 && c == m - 1) {
+            if (r == n-1 && c == m-1) {
                 return dist;
             }
-
-            for (int d = 0; d < 4; d++) {
+            for (int d=0; d<4; d++) {
                 int nr = r + dr[d], nc = c + dc[d];
-                
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1) {
+                if(nr>=0 && nr<n && nc>=0 && nc<m && maps[nr][nc] == 1) {
                     if (!visited[nr][nc]) {
                         visited[nr][nc] = true;
-                        queue.add(new int[]{ nr, nc, dist + 1 });
+                        queue.add(new int[] {nr, nc, dist+1});
                     }
                 }
-            }
+            } 
         }
-        return -1;
+        return answer;
     }
 }
