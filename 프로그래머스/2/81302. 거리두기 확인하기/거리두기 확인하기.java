59284b38a036f0e58d641a44c0c0e0af6f552482
이보윤 <qhdbs0802@naver.com>
2024-08-08 20:43:16 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.17 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
+        int[] answer = new int[5];
         
-        // 각 대기실 별로 거리두기 준수 여부를 확인
-        for (int i = 0; i < answer.length; i++) {
+        for (int i=0; i<answer.length; i++) {
             if (check(places[i])) {
                 answer[i] = 1;
             } else {
@@ -16,11 +15,10 @@ class Solution {
     }
     
     boolean check(String[] place) {
-        for (int r = 0; r < 5; r++) {
-            for (int c = 0; c < 5; c++) {
-	            // 응시자(P) 위치 마다 BFS를 수행
+        for (int r=0; r<5; r++) {
+            for (int c=0; c<5; c++) {
                 if (place[r].charAt(c) == 'P') {
-                    if (!bfs(r, c, place)) {
+                    if(!bfs(r, c, place)) {
                         return false;
                     }
                 }
@@ -30,32 +28,32 @@ class Solution {
     }
     
     boolean bfs(int r, int c, String[] place) {
-        final int[] dr = {  0,  1,  0, -1 };
-        final int[] dc = {  1,  0, -1,  0 };
+        final int[] dr = {0, 1, 0, -1};
+        final int[] dc = {1, 0, -1, 0};
         
         boolean[][] visited = new boolean[5][5];
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{ r, c, 0 });
+        queue.add(new int[] {r, c, 0});
         visited[r][c] = true;
-
+        
         while (!queue.isEmpty()) {
             int[] cur = queue.remove();
             if (cur[2] >= 2) {
                 continue;
             }
             
-            for (int i = 0; i < 4; i++) {
+            for (int i=0; i<4; i++) {
                 int nr = cur[0] + dr[i];
                 int nc = cur[1] + dc[i];
                 int ndist = cur[2]+1;
-                if (nr >= 0 && nr < 5 && nc >= 0 && nc < 5) {
-                    if (!visited[nr][nc] && place[nr].charAt(nc) != 'X') {
-                        if (!visited[nr][nc]) {
-                            // 거리두기를 준수하지 않았기 때문에 False를 반환한다.
-                            if (place[nr].charAt(nc) == 'P') return false;
-                            queue.add(new int[]{ nr, nc, ndist });
-                            visited[nr][nc] = true;    
+                if (nr>=0 && nr<5 && nc>=0 && nc<5) {
+                    if(!visited[nr][nc] && place[nr].charAt(nc) != 'X') {
+                        // 거리두기를 준수하지 않았기 때문에 False를 반환한다.
+                        if (place[nr].charAt(nc) == 'P') {
+                            return false;
                         }
+                        queue.add(new int[]{ nr, nc, ndist });
+                        visited[nr][nc] = true;    
                     }
                 }
             }
