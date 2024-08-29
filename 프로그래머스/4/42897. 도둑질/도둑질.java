a1757c88cf476a27dea99df16cdf423617770b28
이보윤 <qhdbs0802@naver.com>
2024-08-29 21:30:51 +0900

    [level 4] Title: 도둑질, Time: 23.75 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,25 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] money) {
         int answer = 0;
-        int n = money.length;
         
-        int[] dp1 = new int[n];
+        int[] dp1 = new int[money.length];
         dp1[0] = money[0];
         dp1[1] = money[0];
-        for (int i = 2; i < n - 1; i++) {
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);
+        for(int i=2; i<money.length-1; i++) {
+            dp1[i] = Math.max(dp1[i-2] + money[i], dp1[i-1]);
         }
-
         
         int[] dp2 = new int[money.length];
         dp2[0] = 0;
         dp2[1] = money[1];
-        for (int i = 2; i < n; i++) {
-            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + money[i]);
+        for(int i=2; i<money.length; i++) {
+            dp2[i] = Math.max(dp2[i-2] + money[i], dp2[i-1]);
         }
         
-        answer = Math.max(dp1[n-2], dp2[n-1]);
-
+        answer = Math.max(dp1[money.length-2], dp2[money.length-1]);
         return answer;
     }
 }
