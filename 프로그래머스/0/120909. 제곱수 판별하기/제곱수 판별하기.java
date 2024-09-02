f02f33d9a72a5fbc0be2a314a0886a9ae257a9eb
이보윤 <qhdbs0802@naver.com>
2024-09-02 10:36:26 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        for(int i=1; i*i<=n; i++) {
+            if(i*i == n) {
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}
