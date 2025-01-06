0561ca2a0c2428634c2cbf5a3e661f9154c6eca1
이보윤 <qhdbs0802@naver.com>
2025-01-06 23:13:44 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 94.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        int a = common[1] - common[0];
+        int b = common[2] - common[1];
+        
+        if(a ==  b) {
+            answer = common[common.length - 1] + a;
+        } else {
+            int n = common[1] / common[0];
+            answer = common[common.length - 1] * n;
+        }
+        return answer;
+    }
+}
