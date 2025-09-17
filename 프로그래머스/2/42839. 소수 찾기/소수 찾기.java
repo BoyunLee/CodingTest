bb4e0b9dbd9ce3cea25892c311e5171060931e34
이보윤 <qhdbs0802@naver.com>
2025-09-18 01:27:08 +0900

    [level 2] Title: 소수 찾기, Time: 14.40 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,49 +1,47 @@
 import java.util.*;
 
 class Solution {
+    static Set<Integer> result;
+    static String[] number;
+    static boolean[] visited;
+    
     public int solution(String numbers) {
         int answer = 0;
-        int N = numbers.length();
-        int[] arr = new int[N];
-        boolean[] visit = new boolean[N];
-        Set<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(numbers.charAt(i) + "");
+        number = numbers.split("");
+        result = new HashSet<Integer>();
+        visited = new boolean[number.length];
+        
+        dfs("");
+        
+        for(int r:result) {
+            if(isPrime(r)) {
+                answer++;
+            }
         }
-
-        for (int i = 1; i <= N; i++) {
-            backtrack(arr, i, visit, "", 0, set);
-        }
-
-        answer = set.size();
         return answer;
     }
-
-    public void backtrack(int[] nums, int k, boolean[] visit, String x, int depth, Set<Integer> set) {
-        if (x.length() == k) {
-            if (isPrime(Integer.parseInt(x))) {
-                set.add(Integer.parseInt(x));
-            }
-            return;
+    
+    public void dfs(String current) {
+        if(!current.equals("")) {
+            result.add(Integer.parseInt(current));
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                backtrack(nums, k, visit, x + nums[i], depth + 1, set);
-                visit[i] = false;
+        for(int i=0; i<number.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                dfs(current+number[i]);
+                visited[i] = false;
             }
         }
     }
-
-    public boolean isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
-        int sqrt = (int) Math.sqrt(n);
-        for (int i = 3; i <= sqrt; i += 2) {
-            if (n % i == 0) return false;
+    
+    public boolean isPrime(int num) {
+        if(num < 2) {
+            return false;
+        }
+        for(int i=2; i*i<=num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
         }
         return true;
     }
