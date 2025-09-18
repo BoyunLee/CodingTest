7050aafbfe17931baa7dddcfad084ea8bbb23a40
이보윤 <qhdbs0802@naver.com>
2025-09-19 00:46:07 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -17,7 +17,12 @@ class Solution {
         for(int i = 0; i < n; i++) {
             int next = i + 1;
             while(next < n && name.charAt(next) == 'A') next++;
-            move = Math.min(move, i + n - next + Math.min(i, n - next));
+            
+            // i까지 갔다가 돌아가서 끝 처리
+            move = Math.min(move, i * 2 + (n - next));
+
+            // 뒤쪽부터 처리하다가 돌아오기
+            move = Math.min(move, (n - next) * 2 + i);
         }
 
         answer += move;
