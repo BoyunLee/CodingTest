8b9c215311374a99e0e023b6e889d1300f218c37
이보윤 <qhdbs0802@naver.com>
2025-09-19 23:12:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.59 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,25 +2,21 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-
-        // 결과를 담을 리스트
-        List<Integer> answerList = new ArrayList<>();
-
-        // 첫 번째 원소 추가
-        answerList.add(arr[0]);
+        List<Integer> list = new ArrayList<>();
         
-        // 중복 제거하면서 리스트에 추가
-        for (int i = 1; i < arr.length; i++) {
-            // 현재 결과 리스트의 마지막 원소와 현재 원소가 다른 경우에만 추가
-            if (answerList.get(answerList.size() - 1) != arr[i]) {
-                answerList.add(arr[i]);
+        list.add(arr[0]);
+        
+        for(int i=1; i<arr.length; i++) {
+            int n = list.size();
+            if(list.get(n-1) != arr[i]) {
+                list.add(arr[i]);
             }
         }
         
-        // 리스트를 배열로 변환하여 반환
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
+        int[] answer = new int[list.size()];
+        
+        for(int j=0; j<list.size(); j++) {
+            answer[j] = list.get(j);
         }
 
         return answer;
