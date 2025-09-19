17605d3dabfa2bdc17879346e4aa80711cac6454
이보윤 <qhdbs0802@naver.com>
2025-09-19 23:57:52 +0900

    [level 2] Title: 올바른 괄호, Time: 51.29 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,22 +1,26 @@
 class Solution {
-        boolean solution(String s) {
-            boolean answer = true;
-
-            int count = 0;
-
-            for (int i=0; i<s.length(); i++) {
-                if (s.charAt(i) == '('){
-                    count++;
-                } else {
-                    count--;
-                }    
-                if (count < 0) {
-                    answer = false;
-                }
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        String[] slist = s.split("");
+        
+        int count = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(slist[i].equals("(")) {
+                count += 1;
+            } else if(slist[i].equals(")")) {
+                count -= 1;
             }
-            if (count != 0) {
-                answer = false;
+            
+            if(count < 0) {
+                return false;
             }
-            return answer;
         }
+        
+        if(count != 0) {
+            return false;
+        }
+
+        return answer;
     }
+}
