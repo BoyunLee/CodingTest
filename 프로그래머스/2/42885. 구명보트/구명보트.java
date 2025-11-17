fdca6fe44dd15c8aedca7407fc830d95eb09b5f8
이보윤 <qhdbs0802@naver.com>
2025-11-18 02:21:55 +0900

    [level 2] Title: 구명보트, Time: 11.98 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,17 +5,17 @@ class Solution {
         int answer = 0;
         Arrays.sort(people);
         
-        int left = 0;
+        int left=0;
         int right = people.length - 1;
         
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
+        while(left <= right) {
+            if(people[left]+people[right] <= limit) {
                 left++;
             }
             right--;
             answer++;
         }
-        
+
         return answer;
     }
 }
