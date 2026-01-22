1a079d3d83813b49e1c18f907f497a78a6d9ce1d
이보윤 <qhdbs0802@naver.com>
2026-01-22 13:16:55 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 172 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=N; i>0; i--) {
+			System.out.println(" ".repeat(N-i)+"*".repeat(i));
+		}
+	}
+}
