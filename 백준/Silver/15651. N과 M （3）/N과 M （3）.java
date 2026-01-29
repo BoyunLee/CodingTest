957cd4e0705104de8ff6ed66cb1600aadd3f5406
이보윤 <qhdbs0802@naver.com>
2026-01-29 16:11:38 +0900

    [Silver III] Title: N과 M (3), Time: 592 ms, Memory: 72364 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] result = new int[M];
+
+        backtrack(N, M, 0, result);
+        
+        System.out.print(sb.toString());
+    }
+
+    public static void backtrack(int N, int M, int depth, int[] result) {
+    	if (depth == M) {
+    	    for (int i = 0; i < M; i++) {
+    	        sb.append(result[i]).append(' ');
+    	    }
+    	    sb.append('\n');
+    	    return;
+    	}
+    	
+        for (int i = 1; i <= N; i++) {
+        	result[depth] = i;
+            backtrack(N, M, depth+1, result);
+        }
+    }
+}
