329fca95d5a652695fd4598c42cc31d078c1ebf2
이보윤 <qhdbs0802@naver.com>
2026-02-04 17:25:24 +0900

    [Silver II] Title: 섬의 개수, Time: 84 ms, Memory: 13300 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,59 +1,65 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int w;
-    static int h;
-    static int[][] arr;
-    static boolean[][] visited;
-    static int answer;
-    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+	static int w;
+	static int h;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = {-1, 0, 1, 0, -1, -1, 1, 1};
+	static int[] dc = {0, -1, 0, 1, -1, 1, -1, 1};
+	static int answer;
+	
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            w = sc.nextInt();
-            h = sc.nextInt();
-
-            if (w == 0 && h == 0) {
-                break;
-            }
-
-            arr = new int[h][w];
-            visited = new boolean[h][w];
-            answer = 0;
-
-            for(int i=0; i<h; i++) {
-                for(int j=0; j<w; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-
-            for(int i=0; i<h; i++) {
-                for(int j=0; j<w; j++) {
-                    if (arr[i][j] == 1 && !visited[i][j]) {
-                        answer++;
-                        dfs(i, j);
-                    }
-                }
-            }
-            System.out.println(answer);
-        }
-    }
-
-    static void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < h && ny < w) {
-                if(!visited[nx][ny] && arr[nx][ny] == 1) {
-                    dfs(nx, ny);
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			
+			if(w==0 && h==0) {
+				break;
+			}
+			
+			map = new int[h][w];
+			visited = new boolean[h][w];
+			answer = 0;
+			
+			for(int i=0; i<h; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					if(map[i][j] == 1 && !visited[i][j]) {
+						answer++;
+						dfs(i, j);
+					}
+				}
+			}
+			System.out.println(answer);
+ 		}
+	}
+	
+	static void dfs(int r, int c) {
+		visited[r][c] = true;
+		
+		for(int d=0; d<8; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if(nr>=0 && nr<h && nc>=0 && nc<w) {
+				if(!visited[nr][nc] && map[nr][nc] == 1) {
+					dfs(nr, nc);
+				}
+			}
+		}
+	}
 }
