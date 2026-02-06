0e8c43917bbd4d11817549f73b08e74f129491b5
이보윤 <qhdbs0802@naver.com>
2026-02-06 13:50:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1416 ms, Memory: 140072 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,5 +1,6 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
