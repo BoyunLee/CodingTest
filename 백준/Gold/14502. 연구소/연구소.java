9470c8e898d8d3ddd9d59a73519e89e4f1a84b61
이보윤 <qhdbs0802@naver.com>
2026-02-13 11:54:07 +0900

    [Gold IV] Title: 연구소, Time: 348 ms, Memory: 100252 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -9,6 +11,7 @@ public class Main {
 	static int N;
 	static int M;
 	static int[][] map;
+	static List<int[]> virusList;
 	static int[][] copy;
 	static boolean[][] virusVisited;
 	static int[] dr = {0, 1, 0, -1};
@@ -22,34 +25,39 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][M];
+		virusList = new ArrayList<>();
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 2) {
+					virusList.add(new int[] {i, j});
+				}
 			}
 		}
 		
 		answer = 0;
-		block(0);
+		block(0, 0);
 		System.out.println(answer);
 	}
 	
-	static void block(int wall) {
-		if(wall == 3) {
-			int[][] copy = copyMap();
-		    virus(copy); 
-			return;
-		}
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(map[i][j] == 0) {
-					map[i][j] = 1;
-					block(wall+1);
-					map[i][j] = 0;
-				}
-			}
-		}
+	static void block(int start, int wall) {
+	    if (wall == 3) {
+	        int[][] copy = copyMap();
+	        virus(copy);
+	        return;
+	    }
+
+	    for (int idx = start; idx < N * M; idx++) {
+	        int r = idx / M;
+	        int c = idx % M;
+
+	        if (map[r][c] == 0) {
+	            map[r][c] = 1;
+	            block(idx + 1, wall + 1);
+	            map[r][c] = 0;
+	        }
+	    }
 	}
 	
 	private static int[][] copyMap() {
@@ -65,30 +73,33 @@ public class Main {
 
 	static void virus(int[][] copy) {
 		virusVisited = new boolean[N][M];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(copy[i][j] == 2 && !virusVisited[i][j]) {
-					bfs(i, j);
-				}
-			}
+		for(int[] v : virusList) {
+			bfs(v[0], v[1]);
 		}
 		safe();
 	}
 	
 	static void bfs(int r, int c) {
-		virusVisited[r][c] = true;
-		copy[r][c] = 2;
-			
-		for(int d=0; d<4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-			
-			if(0<=nr && nr<N && 0<=nc && nc<M) {
-				if(copy[nr][nc] == 0 && !virusVisited[nr][nc]) {
-					bfs(nr, nc);
-				}
-			}
-		}
+	    Queue<int[]> q = new ArrayDeque<>();
+	    q.offer(new int[]{r, c});
+	    virusVisited[r][c] = true;
+
+	    while(!q.isEmpty()) {
+	        int[] cur = q.poll();
+
+	        for(int d=0; d<4; d++) {
+	            int nr = cur[0] + dr[d];
+	            int nc = cur[1] + dc[d];
+
+	            if(0<=nr && nr<N && 0<=nc && nc<M) {
+	                if(copy[nr][nc] == 0 && !virusVisited[nr][nc]) {
+	                    virusVisited[nr][nc] = true;
+	                    copy[nr][nc] = 2;
+	                    q.offer(new int[]{nr, nc});
+	                }
+	            }
+	        }
+	    }
 	}
 	
 	static void safe() {
