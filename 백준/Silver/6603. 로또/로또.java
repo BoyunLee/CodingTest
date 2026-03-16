1e9ab485a1921fe8941172d752a1b92524e6d41e
이보윤 <qhdbs0802@naver.com>
2026-03-17 00:29:08 +0900

    [Silver II] Title: 로또, Time: 68 ms, Memory: 11800 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -4,50 +4,46 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int[] arr;
-	static boolean[] visited;
-	static int[] answer;
 	static int N;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
 		while(true) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-			if(N == 0) {
-				break;
-			}
+			if(N == 0) break;
 			
 			arr = new int[N];
-			visited = new boolean[N];
 			for(int i=0; i<N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			answer = new int[6];
-			dfs(0, 0);
-			System.out.println();
+			dfs(0, 0, new int[6]);
+			sb.append("\n");
 		}
-
+		System.out.println(sb);
 	}
 	
-	static void dfs(int idx, int cnt) {
-		if(cnt == 6) {
-			for (int i = 0; i < 6; i++) {
-		        System.out.print(answer[i]);
-		        if (i < 5) System.out.print(" ");
-		    }
-		    System.out.println();
-		    return;
+	static void dfs(int idx, int depth, int[] temp) {
+		if(depth == 6) {
+			
+			for(int i=0; i<6; i++) {
+				sb.append(temp[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
 		}
 		
-		if(idx == N) return;
+		if(idx == N) {
+			return;
+		}
 		
-		answer[cnt] = arr[idx];
-		dfs(idx+1, cnt+1);
+		temp[depth]=arr[idx];
 		
-		dfs(idx+1, cnt);
-	} 
+		dfs(idx+1, depth+1, temp);
+		
+		dfs(idx+1, depth, temp);
+	}
 }
