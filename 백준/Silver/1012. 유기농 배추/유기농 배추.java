a0b88a55091ea3732c82a2a89398acd82406208d
이보윤 <qhdbs0802@naver.com>
2026-03-16 17:09:28 +0900

    [Silver II] Title: 유기농 배추, Time: 96 ms, Memory: 13404 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,56 +1,79 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0}; 
-    static int[] dy = {0, 0, -1, 1}; 
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	static StringBuilder sb = new StringBuilder();
+	static int M, N, K, cnt;
+	static int[][] map;
+	static Queue<int[]> queue;
+	static int[][] moves = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
 
-        int text = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			cnt = 0;
 
-        for (int t=0; t<text; t++) {
-            int M = sc.nextInt();
-            int N = sc.nextInt();
-            int K = sc.nextInt();
+			map = new int[N][M];
+			queue = new ArrayDeque<>();
 
-            map = new int[M][N];
-            visited = new boolean[M][N];
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				int col = Integer.parseInt(st.nextToken());
+				int row = Integer.parseInt(st.nextToken());
+				map[row][col] = 1;
+			}
 
-            for (int i=0; i<K; i++) {
-                int x = sc.nextInt();
-                int y = sc.nextInt();
-                map[x][y] = 1;
-            }
+			// bfs 시뮬레이션
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if (map[i][j] == 1) {
+						bfs(i, j);
+						cnt++;
+					}
+				}
+			}
 
-            int count = 0;
+			sb.append(cnt).append("\n");
+		}
+		System.out.println(sb.toString().trim());
 
-            for (int i=0; i<M; i++) {
-                for (int j=0; j<N; j++) {
-                    if(map[i][j] == 1 && !visited[i][j]) {
-                        dfs(i, j, M, N);
-                        count++;
-                    }
-                }
-            }
-            System.out.println(count);
-        }
-    }
+	}
 
-    public static void dfs(int x, int y, int M, int N) {
-        visited[x][y] = true;
+	static void bfs(int row, int col) {
+		queue.offer(new int[] { row, col });
+		map[row][col] = 0;
 
-        for (int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			int r = curr[0];
+			int c = curr[1];
 
-            if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-                if (map[nx][ny] == 1 && !visited[nx][ny]) {
-                    dfs(nx, ny, M, N);
-                }
-            }
-        }
-    }
+			for (int i = 0; i < 4; i++) {
+				int nr = r + moves[i][0];
+				int nc = c + moves[i][1];
+
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M)
+					continue;
+
+				if (map[nr][nc] == 0)
+					continue;
+
+				map[nr][nc] = 0;
+				queue.offer(new int[] { nr, nc });
+			}
+
+		}
+	}
+
 }
