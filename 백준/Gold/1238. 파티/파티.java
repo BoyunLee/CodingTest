f1c11c370d71608561a8bf92c2ef8629d3233a97
이보윤 <qhdbs0802@naver.com>
2026-03-17 11:47:48 +0900

    [Gold III] Title: 파티, Time: 428 ms, Memory: 52624 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,81 +7,97 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N, M, X;
-	static int max_value = 1_000_000;
-
-	static class Edge implements Comparable<Edge> {
-		int to;
-		int weight;
-
-		Edge(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
+	
+	static class Edge {
+		int end;
+		int time;
+		
+		public Edge(int end, int time) {
+			this.end = end;
+			this.time = time;
+		}
+	}
+	
+	static class Node implements Comparable<Node> {
+		int v;
+		int total;
+		
+		public Node(int v, int total) {
+			this.v = v;
+			this.total = total;
 		}
 
 		@Override
-		public int compareTo(Edge e) {
-			return Integer.compare(this.weight, e.weight);
+		public int compareTo(Node o) {
+			return this.total - o.total;
 		}
 	}
+	
+	static List<Edge>[] graph;
+	static int N;
+	static int X;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		X = Integer.parseInt(st.nextToken());
-
-		List<Edge>[] adj = new ArrayList[N + 1];
-		List<Edge>[] reverseAdj = new ArrayList[N + 1];
-
-		for (int i = 0; i <= N; i++) {
-			adj[i] = new ArrayList<>();
-			reverseAdj[i] = new ArrayList<>();
+		
+		graph = new ArrayList[N+1];
+		for(int i=1; i<=N; i++) {
+			graph[i] = new ArrayList<>();
 		}
-
-		for (int i = 0; i < M; i++) {
+		
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-
-			adj[from].add(new Edge(to, weight));
-			reverseAdj[to].add(new Edge(from, weight));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(new Edge(b, c));
 		}
-
-		int[] dist1 = dijkstra(adj);
-		int[] dist2 = dijkstra(reverseAdj);
-
-		int result = 0;
-
+		
+		int[] sum = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			int[] distFromI = dijkstra(i);
+			sum[i] = distFromI[X];
+		}
+		
+		int[] distFromX = dijkstra(X);
 		for (int i = 1; i <= N; i++) {
-			result = Math.max(result, dist1[i] + dist2[i]);
+		    sum[i] += distFromX[i];
 		}
-		System.out.println(result);
+		
+		sum[X] = 0;
+		int max = 0;
+		for(int i=1; i<=N; i++) {
+			max = Math.max(max, sum[i]);
+		}
+		
+		System.out.println(max);
 	}
-
-	static int[] dijkstra(List<Edge>[] adjList) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.offer(new Edge(X, 0));
-		int[] dist = new int[N + 1];
-		Arrays.fill(dist, max_value);
-		dist[X] = 0; // 시작점은 거리 0
-
-		while (!pq.isEmpty()) {
-			Edge curr = pq.poll();
-			int to = curr.to;
-			int weight = curr.weight;
-
-			if (dist[to] < weight)
-				continue;
-
-			for (Edge next : adjList[to]) {
-				if (dist[next.to] > dist[to] + next.weight) {
-					dist[next.to] = dist[to] + next.weight;
-					pq.offer(new Edge(next.to, dist[next.to]));
+	
+	static int[] dijkstra(int start) {
+		int[] dist = new int[N+1];
+		Arrays.fill(dist, Integer.MAX_VALUE/100);
+		dist[start] = 0;
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if(cur.total > dist[cur.v]) continue;
+			
+			for(Edge e : graph[cur.v]) {
+				int next = e.end;
+				int newDist = dist[cur.v] + e.time;
+				
+				if(newDist < dist[next]) {
+					dist[next] = newDist;
+					pq.add(new Node(next, newDist));
 				}
 			}
 		}
