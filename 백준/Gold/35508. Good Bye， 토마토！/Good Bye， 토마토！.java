6f9c6507de3e6c01119427d329b358193a9eac0e
이보윤 <qhdbs0802@naver.com>
2026-04-27 22:14:31 +0900

    [Gold IV] Title: Good Bye, 토마토!, Time: 1768 ms, Memory: 113620 KB -BaekjoonHub

--- 백준/Gold/35508. Good Bye， 토마토！/Good Bye， 토마토！.java
+++ 백준/Gold/35508. Good Bye， 토마토！/Good Bye， 토마토！.java
@@ -1,5 +1,7 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
