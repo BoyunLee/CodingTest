97ae4fe95049749e855ef4469279002f2455e3ee
이보윤 <qhdbs0802@naver.com>
2026-05-13 17:55:40 +0900

    [level 3] Title: 단속카메라, Time: 4.39 ms, Memory: 59.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        int camera = Integer.MIN_VALUE;
+        for(int[] route : routes) {
+            
+            int start = route[0];
+            int end = route[1];
+            
+            if(camera < start) {
+                answer++;
+                camera = end;
+            }
+        }
+        return answer;
+    }
+}
